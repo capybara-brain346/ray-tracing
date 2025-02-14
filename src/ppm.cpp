6c9b81b44f531fd1b5e6fe61a165f40e900dc37b
capybara-piyush <choudhari.piyush@gmail.com>
2025-02-14 13:49:08 +0530

    added vec3 class

--- src/ppm.cpp
+++ src/ppm.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     int img_width = 256;
     int img_height = 256;
 
-    std::cout << "P3\n"
-              << img_width << " " << img_height << "\n255\n";
+    cout << "P3\n"
+         << img_width << " " << img_height << "\n255\n";
 
     for (int j = 0; j < img_height; j++)
     {
-        for (int i = 0; img_width; i++)
+        clog << "\rScanlines remaining: " << (img_height - j) << " " << flush;
+        for (int i = 0; i < img_width; i++) // Fixed condition here
         {
             auto r = double(i) / (img_width - 1);
             auto g = double(j) / (img_height - 1);
@@ -20,7 +23,8 @@ int main()
             int ig = int(255.999 * g);
             int ib = int(255.999 * b);
 
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+    clog << "\r Done."
 }
